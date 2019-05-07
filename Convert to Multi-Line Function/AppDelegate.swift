37cd511261273fd557fa399040518e82edc7709b
Christopher Chute <chutechristopher@gmail.com>
2019-05-06 21:39:20 -0500

    Initial Commit

---
+++ Convert to Multi-Line Function/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Convert to Multi-Line Function
+//
+//  Created by Christopher Chute on 5/6/19.
+//  Copyright © 2019 chute. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Convert to Multi-Line Function/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Convert to Multi-Line Function
+//
+//  Created by Christopher Chute on 5/6/19.
+//  Copyright © 2019 chute. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
